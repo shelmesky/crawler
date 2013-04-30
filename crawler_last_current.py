a70a5f314f6114e53b083b7804b83a23d6e4af13
roy lieu <roy@rootk.com>
2013-04-30 16:21:02 +0800

    splite request into 16

--- crawler_last_current.py
+++ crawler_last_current.py
@@ -3,18 +3,18 @@
 
 import time
 import re
+import httplib
+import urllib
+import random
+import os
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
 import gevent
 from gevent import monkey
-import httplib
-import urlparse
-from gevent import queue
 from gevent.pool import Pool
-from pyquery import PyQuery as pq
-from prettyprint.prettyprint import pp
-import random
-import urllib
-import multiprocessing
-import os
 
 monkey.patch_all()
 
@@ -22,13 +22,13 @@ import sys
 reload(sys)
 sys.setdefaultencoding('UTF-8')
 
-
 keywords_map = None
 relative_map = None
-#ip_pool = ["180.153.152.37", "180.153.152.66"]
-#ip_pool = ["180.153.152.37", "180.153.152.66", "180.153.152.67"]
-ip_pool = ["180.153.152.37"]
-regex = re.compile("relatedSearch.*search\?q=(.*)&", data)
+ip_pool = ["180.153.152.37", "180.153.152.66", "180.153.152.67"]
+#ip_pool = ["180.153.152.37"]
+regex_relative = re.compile("relatedSearch.*search\?q=(.*)&")
+regex_keywords = re.compile("\((.*)\)")
+
 
 def get_random_obj(obj):
     return random.choice(obj)
@@ -46,6 +46,7 @@ def get_keyword_list_url(keyword):
     url = "/sug"
     return hostname, url, urllib.urlencode(parms)
 
+
 def get_query_url(query):
     parms = dict(
         q = query,
@@ -60,46 +61,25 @@ def get_query_url(query):
     return hostname, url, urllib.urlencode(parms)
 
 
-class GetConn(object):
-    conn_map = dict()
-    
-    @staticmethod
-    def get_conn(host):
-        cls = GetConn
-        if host in cls.conn_map:
-            conns = ls.conn_map[host]
-        else:
-            cls.conn_map[host] = list()
-            for ip in ip_pool:
-                conn = httplib.HTTPConnection(host, source_address=(ip, 0))
-                cls.conn_map[host].append(conn)
-            conns = cls.conn_map[host]
-            
-        return get_random_obj(conns)
-
-
 def get_data(hostname, method="GET", url=None, body=None):
-    #conn = httplib.HTTPConnection(hostname)
     conn = httplib.HTTPConnection(hostname, source_address=(get_random_obj(ip_pool), 0))
-    #headers = {"User-Agent": "spider"}
-    headers = {'Accept-Language':'zh-cn','Accept-Encoding': 'gzip, deflate','User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0;Windows NT 5.0)','Connection':' Keep-Alive' } 
+    headers = {'Accept-Language':'zh-cn','Accept-Encoding': 'gzip, deflate', \
+			'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0;Windows NT 5.0)','Connection':' Keep-Alive' } 
     conn.request(method, url + "?" + body, headers=headers)
-    if hostname == "s.taobao.com":
-        data = conn.getresponse().read()
-    else:
-        data = conn.getresponse().read()
+    data = conn.getresponse().read()
     conn.close()
     return data
 
 
 def get_keyword_list_data(data):
     temp = dict()
-    result = re.findall("\(.*\)", data)
-    keywords = eval(result[0])['result']
+    result = regex_keywords.findall(data)
+    keywords = json.loads(result[0])['result']
     if keywords:
         for key in keywords:
-            temp[key[0]] = key[1]
+            temp[urllib.unquote(key[0])] = key[1]
     return temp
+
 
 def get_keyword_list(keyword):
     hostname, url, parms = get_keyword_list_url(keyword)
@@ -110,9 +90,9 @@ def get_keyword_list(keyword):
 
 def get_relative_list_data(data):
     temp = dict()
-    r = regex.findall(data)
+    r = regex_relative.findall(data)
     for i in r:
-        temp[i] = None
+        temp[urllib.unquote(i)] = None
     return temp
     
     
@@ -139,16 +119,17 @@ def get_final_keywords(keyword):
     keywords = get_keyword_list_data(data)
     
     final_keywords[keyword] = relative.keys() + keywords.keys()
-    
 
 
 if __name__ == '__main__':
     print "PID: ", os.getpid()
     master_key = sys.argv[1]
+    f = lambda i,s: [i[x:x+s] for x in xrange(0, len(i), s)]
+    split_size = 16
     
     start = time.time()
     
-    pool = Pool(size=1024)
+    pool = Pool(size=32)
     
     pool.add(gevent.spawn(get_keyword_list, master_key))
     pool.add(gevent.spawn(get_relative_list, master_key))
@@ -157,14 +138,17 @@ if __name__ == '__main__':
 
     ##########################################################
 
-    [pool.add(pool.spawn(get_final_keywords, key)) for key in keywords_map.keys()]
-    [pool.add(pool.spawn(get_final_keywords, key)) for key in relative_map.keys()]
-    pool.join()
+    allkeys = keywords_map.keys() + relative_map.keys()
+    random.shuffle(allkeys)
+    splited = f(allkeys, split_size)
+    for keys in splited:
+        pool = Pool(split_size)
+        [pool.add(pool.spawn(get_final_keywords, key)) for key in keys]
+        pool.join()
 
     ##########################################################
 
     def decode_print(v):
-        return
         try:
             print v.decode('GBK')
         except:
@@ -173,10 +157,10 @@ if __name__ == '__main__':
     count = 1
     last_list = list()
     for k,v in final_keywords.items():
-        decode_print(k)
+        #decode_print(k)
         #print "-" * 10
         for i in v:
-            decode_print(i)
+            #decode_print(i)
             last_list.append(i)
             count += 1
         #print
@@ -188,8 +172,7 @@ if __name__ == '__main__':
 
     ##########################################################
 
-    split_size = 18
-    f = lambda i,s: [i[x:x+s] for x in xrange(0, len(i), s)]
+    random.shuffle(last_list)
     splited = f(last_list, split_size)
 
     for i in splited:
@@ -201,3 +184,4 @@ if __name__ == '__main__':
 
     end = time.time()
     print "全部完成，整个过程消耗时间：%.2f 秒" % float(end-start)
+
