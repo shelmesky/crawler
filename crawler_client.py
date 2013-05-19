f6ffd9bdd7f7218fc22432e1d7060fab3b49ead5
roy <roy@roy-laptop.(none)>
2013-05-19 22:32:02 +0800

    add client in sync mode

--- crawler_client.py
+++ crawler_client.py
@@ -6,14 +6,26 @@ import gevent
 from gevent.server import StreamServer
 
 
-def read_one_request(sock):
-    response = []
-    while True:
-        data = sock.recv()
+def handle_one_request(data, socket_fileobj):
+    fileobj = socket_fileobj
+    print data
+    fileobj.write(data)
+    fileobj.flush()
 
 
 def stream_handler(sock, addr):
-    pass
+    fileobj = sock.makefile()
+    fileobj.write("Welcome to server!\r\n")
+    fileobj.flush()
+    request = []
+    while True:
+        data = fileobj.readline()
+        if not data: break
+        if data.strip().lower() == "quit": break
+        request.append(data)
+        if "".join(request[-2:]) == "\r\n\r\n":
+            gevent.spawn(handle_one_request, "".join(request[:-2]), fileobj)
+            request = []
 
 
 if __name__ == '__main__':
@@ -21,5 +33,6 @@ if __name__ == '__main__':
         port = sys.argv[1]
     except IndexError:
         port = 9001
+    print "Server listen on port: ", port
     server = StreamServer(("0.0.0.0", port), stream_handler)
     server.serve_forever()
