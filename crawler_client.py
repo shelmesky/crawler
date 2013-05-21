475bf04e1888e420057143d25ab3c72bcda08bf3
roy.lieu <roy.lieu@gmail.com>
2013-05-21 17:38:52 +0800

    add read_delimiter and read_size and clear

--- crawler_client.py
+++ crawler_client.py
@@ -13,40 +13,134 @@ class TCPHandler(object):
         self.sock = sock
         self.fileobj = sock.makefile()
         self.address = address
-        self.read_buf = []
+        self.read_buf = deque()
+        self.read_buf_size = 0
+        self.closed = False
         self.request()
     
-    def handle_one_request(self, data):
-        print "".join(data).strip()
+    def handle_headers(self, headers):
+        content_length = headers.get("content-length", 0)
+        try:
+            content_length = int(content_length)
+        except ValueError:
+            raise RuntimeError("Invalid content-length.")
+        
+        if content_length:
+            while True:
+                body = self.buffer_get_size(content_length)
+                if body:
+                    self.handle_body(body)
+                    break
+                
+                if not self.read_to_buffer():
+                    self.close()
+                    break
+    
+    def handle_body(self, body):
+        print self.headers
+        print len(body)
+        self.clear()
     
     def parse_headers(self, data):
-        pass
+        try:
+            lines = data.splitlines()[:-1]
+            headers = {}
+            for line in lines:
+                key, value = line.split(":")
+                headers[key.strip()] = value.strip()
+        except ValueError:
+            raise RuntimeError("Malformed request line")
+        self.headers = headers
+        return headers
     
     def request(self):
         while True:
-            # readline()是调用patch之后标准库中的socket模块的_fileobj类的readline
-            # 原理是每一次recv(1)，并检测行结束符
-            data = self.sock.recv(4096)
-            
-            # 因为_fileobj仅仅是将socket接口封装成了类文件接口
-            # 并增加了读写缓冲
-            # 单并没有对客户端断开做任何检测，是很底层的封装
-            # 所以这里需要对读取不到数据的连接做检测
-            # 动作为断开连接(因为函数执行完毕，即greelnet执行完毕)
-            # python执行GC，greenlet对象被销毁，socket连接断开
-            if not data: break
-            
-            if data.strip().lower() == "quit": break
-            
-            self.read_buf.append(data)
-            
             # 如果检测到client的行结束符
-            if "".join(self.read_buf[-2:]) == "\r\n\r\n":
-                gevent.spawn(self.handle_one_request, self.read_buf[:-2])
-                self.read_buf = []
+            data = self.buffer_get_delimiter("\r\n\r\n")
+            if data:
+                #gevent.spawn(self.handle_request, data)
+                headers = self.parse_headers(data)
+                self.handle_headers(headers)
+                
+            # 如果读不到数据，说明client已经关闭连接
+            # server此时也应该关闭
+            if not self.read_to_buffer():
+                self.close()
+                break
+
+    def read_to_buffer(self):
+        # readline()是调用patch之后标准库中的socket模块的_fileobj类的readline
+        # 原理是每一次recv(1)，并检测行结束符
+        # sock.recv()是调用C的socket模块 是syscall
+        #data = self.fileobj.readline()
+        if not self.closed:
+            data = self.sock.recv(4096)
+            if not data:
+                return
+            self.read_buf.append(data)
+            data_length = len(data)
+            self.read_buf_size += data_length
+            return data_length
     
-    def _consume(self, deque, size):
-        pass
+    def close(self):
+        self.sock.close()
+        self.closed = True
+    
+    def clear(self):
+        self.read_buf.clear()
+        self.read_buf_size = 0
+        self.headers = None
+
+    def buffer_get_size(self, size):
+        if self.read_buf and self.read_buf_size >= size:
+            return self._consume(size)
+        return False
+    
+    def buffer_get_delimiter(self, delimiter):
+        if self.read_buf:
+            while True:
+                loc = self.read_buf[0].find(delimiter)
+                if loc != -1:
+                    delimiter_len = len(delimiter)
+                    return self._consume(loc + delimiter_len)
+                if self.buffer_len() == 1:
+                    break
+                _double_prefix(self.read_buf)
+        return False
+    
+    def buffer_len(self):
+        return len(self.read_buf)
+    
+    def _consume(self, loc):
+        if loc == 0:
+            return ""
+        _merge_prefix(self.read_buf, loc)
+        self.read_buf_size -= loc
+        return self.read_buf.popleft()
+    
+    
+def _merge_prefix(deque, size):
+    if len(deque) == 1 and len(deque[0]) <= size:
+        return
+    prefix = []
+    remaining = size
+    while deque and remaining > 0:
+        chunk = deque.popleft()
+        if len(chunk) > remaining:
+            deque.appendleft(chunk[remaining:])
+            chunk = chunk[:remaining]
+        prefix.append(chunk)
+        remaining -= len(chunk)
+    if prefix:
+        deque.appendleft(type(prefix[0])().join(prefix))
+    if not deque:
+        deque.appendleft(b"")
+
+
+def _double_prefix(deque):
+    new_len = max(len(deque[0]) * 2,
+                  (len(deque[0]) + len(deque[1])))
+    _merge_prefix(deque, new_len)
 
 
 def stream_handler(sock, address):
@@ -64,3 +158,4 @@ if __name__ == '__main__':
         server.serve_forever()
     except KeyboardInterrupt:
         print "Server exit..."
+        server.stop()
